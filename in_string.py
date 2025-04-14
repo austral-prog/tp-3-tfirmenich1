a10eedbc47b5afafa6ba6e69b6df515bd8535634
tfirmenich1 <tfirmenich@mail.austral.edu.ar>
2025-04-14 08:47:41 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,18 +1,13 @@
 def check_vowels():
-        # Código a implementar utilizando input.
-	nombre = input(("Ingresa tu nombre: ").lower())
-	
-	print("Contiene a:", "a" in nombre or "A" in nombre)
-	print("Contiene e:", "e" in nombre or "E" in nombre)
-	print("Contiene i:", "i" in nombre or "I" in nombre)
-	print("Contiene o:", "o" in nombre or "O" in nombre)
-	print("Contiene u:", "u" in nombre or "U" in nombre)
+    # Código a implementar utilizando input.
+    nombre = input("Ingrese nombre: ").lower()
+    
+    print ( "Contiene a:", "a" in nombre or "A" in nombre)
+    print ( "Contiene e:", "e" in nombre or "E" in nombre)
+    print ( "Contiene i:", "i" in nombre or "I" in nombre)
+    print ( "Contiene o:", "o" in nombre or "O" in nombre)
+    print ( "Contiene u:", "u" in nombre or "U" in nombre)
 
- def slice_simple():
-    texto = "Awesome".lower()
 
-    print (texto[0:3])
-    print (texto[2:5])
-    print (texto[0:4]+texto[4:7])
-
-slice_simple()
+# Para verificar este ejercicio ejecutar el comando
+# `pytest tp3_in_string_test.py` o `python tp3_in_string_test.py`
